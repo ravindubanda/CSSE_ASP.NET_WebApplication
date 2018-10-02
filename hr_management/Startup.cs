1fb4f66502b105c49c039417210de0a620c0533b
Asus <sithara.liyanaarachchi@gmail.com>
2018-10-02 19:53:59 +0530

    Add project files.

---
+++ hr_management/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(hr_management.Startup))]
+namespace hr_management
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
